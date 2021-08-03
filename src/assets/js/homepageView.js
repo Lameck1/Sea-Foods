83147bcd5e0b3203274789f19e44eeb3a47a5542
olawale-o <omoogunolawale@gmail.com>
2021-08-03 15:46:02 +0100

    Import footer in hompageView.js file

--- src/assets/js/homepageView.js
+++ src/assets/js/homepageView.js
@@ -1,11 +1,12 @@
 import createElement from './helpers/createElement';
 import Logo from '../img/logo.png';
-import { header } from './dom.utils';
+import { header, footer } from './dom.utils';
 
 export default () => {
   const navigation = header(Logo);
+  const bottom = footer();
   const main = createElement('main', { class: 'main d-flex' });
   const foodList = createElement('ul', { class: 'food-list d-flex' });
   main.append(foodList);
-  document.body.append(navigation, main);
+  document.body.append(navigation, main, bottom);
 };
