ca172379357b758a45f38aac1a0727cef85d62e1
olawale-o <omoogunolawale@gmail.com>
2021-08-03 15:16:27 +0100

    Import Logo and header function in homepageView.js file

--- src/assets/js/homepageView.js
+++ src/assets/js/homepageView.js
@@ -1,8 +1,11 @@
 import createElement from './helpers/createElement';
+import Logo from '../img/logo.png';
+import { header } from './dom.utils';
 
 export default () => {
+  const navigation = header(Logo);
   const main = createElement('main', { class: 'main d-flex' });
   const foodList = createElement('ul', { class: 'food-list d-flex' });
   main.append(foodList);
-  document.body.append(main);
+  document.body.append(navigation, main);
 };
