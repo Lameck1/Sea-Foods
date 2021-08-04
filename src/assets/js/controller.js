cd824090ec5e1ca544bb41f270d0ca5a61da1bb0
Lameck1 <lameck72@gmail.com>
2021-08-04 11:09:40 +0300

    Add new Like
    The #postLike and #handlePostLike methods ensures that when the user clicks on the Like button of an item (on Homepage), the interaction is recorded in the Involvement API and the screen is updated.

--- src/assets/js/controller.js
+++ src/assets/js/controller.js
@@ -1,14 +1,29 @@
 import getMeals from './getMeals';
-import { getLikes } from './helpers/likes';
+import getElement from './helpers/getElement';
+import { getLikes, postLike } from './helpers/likes';
 import listFoods from './helpers/listFoods';
 import homepageView from './homepageView';
 
 export default () => ({
+  async handlePostLike() {
+    getElement('.food-list').addEventListener('click', async (event) => {
+      event.preventDefault();
+      if (event.target.className === 'like-icon') {
+        const id = event.target.getAttribute('data-id');
+        await postLike(id);
+        const meals = await getMeals();
+        const likes = await getLikes();
+        listFoods(meals, likes);
+      }
+    });
+  },
+
   async init() {
     const meals = await getMeals();
     const likes = await getLikes();
     homepageView();
     listFoods(meals, likes);
     getMeals();
+    this.handlePostLike();
   },
 });
